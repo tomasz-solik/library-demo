8b91a237d646f29d97295b6e2e55f1270b628a63
Tomasz Solik <tomaszsolik@gmail.com>
2026-07-16 20:35:12 +0200

    Production deploy fix

--- docker/prod/start.sh
+++ docker/prod/start.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+echo "Fix permissions..."
+
+chmod -R 777 var
 
 echo "Running migrations..."
 
 php bin/console doctrine:migrations:migrate \
     --no-interaction || true
+
+
+echo "Clearing cache..."
+
+php bin/console cache:clear --env=prod
 
 
 echo "Starting php-fpm..."
