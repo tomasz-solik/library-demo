60d3478b2bbefdbfec1bc93c50b694137dfb0c9e
Tomasz Solik <tomaszsolik@gmail.com>
2026-07-17 13:07:45 +0200

    Devops Update

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,27 +2,23 @@
 
 set -e
 
-echo "[TOMSOL]Installing dependencies..."
-
+echo "[TOMSOL] Installing dependencies..."
 composer install \
     --no-interaction \
     --prefer-dist \
     --no-progress
 
 
-echo "[TOMSOL]Waiting for database..."
-
+echo "[TOMSOL] Waiting for database..."
 until php bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1
 do
     sleep 2
 done
 
 
-echo "[TOMSOL]Running migrations..."
-
+echo "[TOMSOL] Running migrations..."
 php bin/console doctrine:migrations:migrate --no-interaction
 
 
-echo "[TOMSOL]Starting PHP-FPM..."
-
+echo "[TOMSOL] Starting PHP-FPM..."
 exec php-fpm
--- docker/prod/start.sh
+++ docker/prod/start.sh
@@ -2,18 +2,24 @@
 
 set -e
 
-echo "[TOMSOL]PHP modules:"
+echo "[TOMSOL] PHP modules:"
 php -m | grep -E "pdo|pgsql"
 
-echo "[TOMSOL]Waiting for database..."
-php bin/console doctrine:database:create --if-not-exists
+echo "[TOMSOL] DATABASE:"
+echo $DATABASE_URL
 
-echo "[TOMSOL]Running migrations..."
-php bin/console doctrine:migrations:migrate --no-interaction
+echo "[TOMSOL] Database check..."
+php bin/console doctrine:database:create \
+    --if-not-exists \
+    --env=prod
 
-echo "[TOMSOL]Starting php-fpm..."
+echo "[TOMSOL] Migrations..."
+php bin/console doctrine:migrations:migrate \
+    --no-interaction \
+    --env=prod
+
+echo "[TOMSOL] Starting php-fpm..."
 php-fpm -D
 
-echo "[TOMSOL]Starting nginx..."
-
+echo "[TOMSOL] Starting nginx..."
 nginx -g "daemon off;"
